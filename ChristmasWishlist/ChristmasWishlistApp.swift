4129a3ad3375df092286ac628c9b7ee43c887773
designoverhaul <aaronheine@gmail.com>
2025-11-16 09:41:48 -0500

    Initial Commit

---
+++ ChristmasWishlist/ChristmasWishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasWishlistApp.swift
+//  ChristmasWishlist
+//
+//  Created by Aaron Heine on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasWishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChristmasWishlistTests/ChristmasWishlistTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasWishlistTests.swift
+//  ChristmasWishlistTests
+//
+//  Created by Aaron Heine on 11/16/25.
+//
+
+import Testing
+@testable import ChristmasWishlist
+
+struct ChristmasWishlistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
